8c61dfca306b773e3bb37d00938ba027574d6ba8
kokop-dev <pnmmdata2639@gmail.com>
2026-09-01 22:41:03 +0100

    feat: add MarqueeTicker component

--- tailwind.config.js
+++ tailwind.config.js
@@ -101,7 +101,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'marquee': 'marquee 28s linear infinite'
+        'marquee': 'marquee 56s linear infinite'
       }
     }
   },
