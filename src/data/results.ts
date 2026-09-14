e0150888eff9abf3af2d2113607d58aa34936555
kokop-dev <pnmmdata2639@gmail.com>
2026-09-14 23:44:46 +0100

    feat: add Results page

---
+++ src/data/results.ts
@@ -0,0 +1,285 @@
+export type Result = {
+	name: string;
+	description: string;
+	date: string;
+    url: string;
+};
+
+export const results2026: Result[] = [
+	{
+		name: "Cornish Tri Series 1",
+		description: "Wheal Jane",
+		date: "2026-03-22",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5160&EId=6",
+	},
+	{
+		name: "Cornish Tri Series 2",
+		description: "Wadebridge",
+		date: "2026-04-26",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5162",
+	},
+	{
+		name: "Cornish Tri Series 3",
+		description: "Marazion",
+		date: "2026-05-30",
+		url: "https://www.chiptimingresults.co.uk/Results.aspx?CId=61&RId=5164",
+	},
+    {
+		name: "Cornish Tri Series 4",
+		description: "Tavistock",
+		date: "2026-07-12",
+		url: "https://www.chiptimingresults.co.uk/Results.aspx?CId=61&RId=5167",
+	},
+	{
+		name: "Cornish Tri Series 5",
+		description: "Bodmin",
+		date: "2026-08-23",
+		url: "https://www.chiptimingresults.co.uk/Results.aspx?CId=61&RId=5168",
+	},
+	{
+		name: "Cornish Tri Series 6",
+		description: "Perranporth",
+		date: "2026-09-06",
+		url: "https://www.chiptimingresults.co.uk/Results.aspx?CId=61&RId=5169",
+	},
+    {
+		name: "Cornish Tri Series 7",
+		description: "Falmouth",
+		date: "2026-09-20",
+		url: "",
+	},
+    {
+		name: "Taunton School Junior Triathlon",
+		description: "",
+		date: "2026-06-21",
+		url: "https://intotri.com/wp-content/uploads/2026/06/Taunton-School-Triathlon-2026-Results.pdf",
+	},
+    {
+		name: "Selsey Triathlon",
+		description: "",
+		date: "2026-10-04",
+		url: "",
+	},
+];
+
+export const results2025: Result[] = [
+	{
+		name: "Cornish Tri Series 1",
+		description: "Par",
+		date: "2025-03-29",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5131",
+	},
+    {
+		name: "Cornish Tri Series 2",
+		description: "Wadebridge",
+		date: "2025-04-27",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5134",
+	},
+    {
+		name: "Cornish Tri Series 3",
+		description: "Marazion",
+		date: "2025-05-31",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5138",
+	},
+    {
+		name: "Cornish Tri Series 4",
+		description: "Tavistock",
+		date: "2025-07-13",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5142",
+	},
+    {
+		name: "Cornish Tri Series 5",
+		description: "Bodmin",
+		date: "2025-08-24",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5145",
+	},
+    {
+		name: "Cornish Tri Series 6",
+		description: "Perranporth",
+		date: "2025-09-07",
+		url: "https://chiptimingresults.co.uk/results.aspx?CId=61&RId=5147",
+	},
+    {
+		name: "Cornish Tri Series 7",
+		description: "Falmouth",
+		date: "2025-09-21",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5148",
+	},
+    {
+		name: "Taunton School Junior Triathlon",
+		description: "",
+		date: "2025-06-15",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5140",
+	},
+];
+
+export const results2024: Result[] = [
+	{
+		name: "Cornish Tri Series 1",
+		description: "Par",
+		date: "2024-04-06",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5106",
+	},
+    {
+		name: "Cornish Tri Series 2",
+		description: "Wadebridge",
+		date: "2024-05-05",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5109",
+	},
+    {
+		name: "Cornish Tri Series 3",
+		description: "Newquay",
+		date: "2024-06-02",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5110",
+	},
+    {
+		name: "Cornish Tri Series 4",
+		description: "Portreath",
+		date: "2024-06-29",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5116",
+	},
+    {
+		name: "Cornish Tri Series 5",
+		description: "Tavistock",
+		date: "2024-07-14",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5117",
+	},
+    {
+		name: "Cornish Tri Series 6",
+		description: "Bodmin",
+		date: "2024-08-25",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5120",
+	},
+    {
+		name: "Cornish Tri Series 7",
+		description: "Falmouth",
+		date: "2024-09-22",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5122",
+	},
+    {
+		name: "Taunton School Junior Triathlon",
+		description: "",
+		date: "2024-06-23",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5115",
+	},
+];
+
+export const results2023: Result[] = [
+	{
+		name: "Cornish Tri Series 1",
+		description: "Carn Brea",
+		date: "2023-04-23",
+		url: "https://chiptimingresults.co.uk/results.aspx?CId=61&RId=5082",
+	},
+    {
+		name: "Cornish Tri Series 2",
+		description: "Wadebridge",
+		date: "2023-05-14",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5083",
+	},
+    {
+		name: "Cornish Tri Series 3",
+		description: "Newquay",
+		date: "2023-06-18",
+		url: "https://chiptimingresults.co.uk/results.aspx?CId=61&RId=5089",
+	},
+    {
+		name: "Cornish Tri Series 4",
+		description: "Tavistock",
+		date: "2023-07-16",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5092",
+	},
+    {
+		name: "Cornish Tri Series 5",
+		description: "Bodmin",
+		date: "2023-08-23",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5094",
+	},
+    {
+		name: "Cornish Tri Series 6",
+		description: "Falmouth",
+		date: "2023-09-24",
+		url: "https://chiptimingresults.co.uk/results.aspx?CId=61&RId=5098",
+	},
+];
+
+export const results2022: Result[] = [
+	{
+		name: "Cornish Tri Series 1",
+		description: "Par",
+		date: "2022-04-03",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5055",
+	},
+    {
+		name: "Cornish Tri Series 2",
+		description: "Carn Brea",
+		date: "2022-05-08",
+		url: "https://chiptimingresults.co.uk/results.aspx?CId=61&RId=5057",
+	},
+    {
+		name: "Cornish Tri Series 3",
+		description: "Newquay",
+		date: "2022-06-19",
+		url: "https://chiptimingresults.co.uk/results.aspx?CId=61&RId=5060&EId=4",
+	},
+    {
+		name: "Cornish Tri Series 4",
+		description: "Tavistock",
+		date: "2022-07-10",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5063",
+	},
+    {
+		name: "Cornish Tri Series 5",
+		description: "Bodmin",
+		date: "2022-08-29",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5066",
+	},
+    {
+		name: "Cornish Tri Series 6",
+		description: "Falmouth",
+		date: "2022-09-25",
+		url: "https://chiptimingresults.co.uk/Results.aspx?CId=61&RId=5072",
+	},
+];
+
+export const results2021: Result[] = [
+	{
+		name: "Cornish Tri Series 1",
+		description: "Wadebridge",
+		date: "2021-05-23",
+		url: "https://www.chiptimingresults.co.uk/results.aspx?CId=61&RId=5032&EId=1",
+	},
+    {
+		name: "Cornish Tri Series 2",
+		description: "Newquay",
+		date: "2021-06-13",
+		url: "https://www.chiptimingresults.co.uk/results.aspx?CId=61&RId=5033&EId=4",
+	},
+    {
+		name: "Cornish Tri Series 3",
+		description: "Tavistock",
+		date: "2021-07-04",
+		url: "https://www.chiptimingresults.co.uk/results.aspx?CId=61&RId=5037&EId=1",
+	},
+    {
+		name: "Cornish Tri Series 4",
+		description: "Bodmin",
+		date: "2021-08-30",
+		url: "https://www.chiptimingresults.co.uk/results.aspx?CId=61&RId=5042",
+	},
+    {
+		name: "Cornish Tri Series 5",
+		description: "Falmouth",
+		date: "2021-09-26",
+		url: "https://www.chiptimingresults.co.uk/Results.aspx?CId=61&RId=5050",
+	},
+];
+
+export const resultYears = [
+	{ year: 2026, results: results2026 },
+	{ year: 2025, results: results2025 },
+	{ year: 2024, results: results2024 },
+	{ year: 2023, results: results2023 },
+	{ year: 2022, results: results2022 },
+	{ year: 2021, results: results2021 },
+] as const;
