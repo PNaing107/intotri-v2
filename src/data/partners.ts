3f6a658df4a6388432a9300a96ad50dab19dea11
kokop-dev <pnmmdata2639@gmail.com>
2026-09-15 15:00:40 +0100

    feat: add partner page

---
+++ src/data/partners.ts
@@ -0,0 +1,21 @@
+export type Partner = {
+	name: string;
+	slogan: string;
+	description: string;
+	url: string;
+};
+
+export const partners: Partner[] = [
+	{
+		name: "Healy's Cyder",
+		slogan: "Award Winning Cyder",
+		description: "Deep in the heart of the Cornish countryside lives a family-run farm that’s been lovingly making award-winning cyders, wines, spirits, juices, preserves, and sauces for 40 years. Whether it’s getting to the core of great flavour or thinking outside the box, they know what it takes to make apples special.",
+		url: "assets/partners/healeys.jpg",
+	},
+	{
+		name: "Giant Helston",
+		slogan: "Ride Giant. Ride Cornwall.",
+		description: "Giant Store Helston sits in the heart of Cornwall, at the gateway to the Lizard peninsula. As one of Cornwall’s largest independent bicycle shops & triathlon specialists, their friendly team of cyclists have you covered with expertise, experience and local knowledge.",
+		url: "assets/partners/Giant-Helston-logo1.png",
+	},
+];
